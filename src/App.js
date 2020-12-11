007cef7813bf33c31c49bfb90bca8ab0a6c677fe
ahmed ragap <ahmed.ragap2414@gmail.com>
2020-12-11 22:10:16 +0200

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ state={
     const city=e.target.elements.city.value; 
     const country=e.target.elements.country.value; 
     const API_Key = "e36ed364400282e43250b6c4c0274d44";
-    const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_Key}`);
+    const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_Key}`);
     const data = await api.json();
     
     if (city && country) {
@@ -51,7 +51,7 @@ state={
 
   render(){
   return (
-    <div className="App">
+    <div className="App"> 
         <div className='form_container'>
        <Form GetWeather={this.GetWeather} Change={this.Change}/>
 
@@ -59,7 +59,7 @@ state={
         temperature ={this.state.temperature}
         city = {this.state.city}
         country = {this.state.country}
-        description = {this.state.description}
+        description = {this.state.description} 
         humidity = {this.state.humidity}
         error = {this.state.error}
       />
